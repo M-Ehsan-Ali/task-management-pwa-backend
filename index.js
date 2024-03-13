141c7dd70c3bfe41f878bf0ce7931b6967b9a4be
M-Ehsan-Ali <mrehsa51@gmail.com>
2024-03-13 14:47:05 +0500

    cors issue resolved

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ const PORT = process.env.PORT || 3001;
 app.use(bodyParser.json());
 
 // CORS configuration
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000/login",
+  })
+);
 
 // Connect to MongoDB
 mongoose
