a78cbff9baedc1acd26bcef660f2b5bfe29d0d6c
M-Ehsan-Ali <mrehsa51@gmail.com>
2024-03-13 14:45:28 +0500

    cors issue resolved

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
-// index.js
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(bodyParser.json());
-const corsOptions = {
-  origin: "http://localhost:3000",
-  methods: ["GET", "POST", "PUT", "DELETE"], // Add the HTTP methods your frontend uses
-  allowedHeaders: ["Content-Type", "Authorization"], // Add the headers your frontend sends
-};
-app.use(cors(corsOptions));
+
+// CORS configuration
+app.use(cors());
 
 // Connect to MongoDB
 mongoose
